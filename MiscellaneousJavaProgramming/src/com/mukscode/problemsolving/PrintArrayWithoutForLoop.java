c4e83f50bdd32e410133ff0b4f0a8ae3c376265b
Mukesh Kumar <mukku111@in.ibm.com>
2021-01-06 21:41:56 +0530

    Print an array without iterating through it.

---
+++ MiscellaneousJavaProgramming/src/com/mukscode/problemsolving/PrintArrayWithoutForLoop.java
@@ -0,0 +1,15 @@
+package com.mukscode.problemsolving;
+
+import java.util.Arrays;
+
+public class PrintArrayWithoutForLoop {
+
+	public static void main(String[] args) {
+		
+		int[] intArray = new int[] {12, 34, 45, 67, 89};
+		
+		System.out.println(Arrays.toString(intArray));
+
+	}
+
+}
