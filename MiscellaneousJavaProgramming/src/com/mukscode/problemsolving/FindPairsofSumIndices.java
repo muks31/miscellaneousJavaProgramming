c66c571b0df8f03ad1b45b72c526f01bb2b0b5a1
Mukesh Kumar <mukeshrai313@gmail.com>
2021-08-11 12:11:33 +0530

    Update FindPairsofSumIndices.java

--- MiscellaneousJavaProgramming/src/com/mukscode/problemsolving/FindPairsofSumIndices.java
+++ MiscellaneousJavaProgramming/src/com/mukscode/problemsolving/FindPairsofSumIndices.java
@@ -10,18 +10,19 @@ public class FindPairsofSumIndices {
 
 	}
 	public static int[] twoSum(int[] nums, int target) {
-		HashMap<Integer, Integer> map = new HashMap<>();
-		for(int i = 0; i<nums.length; i++){
-			map.put(nums[i], i);
-		}
+		Map<Integer, Integer> map = new HashMap<>();
+       		for(int i = 0; i<nums.length; i++){
+            		int delta = target - nums[i];
+            
+           		 if(map.containsKey(delta)){
+               			 return new int[]{i,map.get(delta)};
+           		 }
+           		 else{
+                		map.put(nums[i],i);
+            		}
+      		  }
+        	throw new IllegalArgumentException("No Match Found");
 
-		for(int i =0; i<nums.length; i++){
-			int complement = target - nums[i];
-			if(map.containsKey(complement) && map.get(complement) != i){
-				return new int[]{i, map.get(complement)};
-			}
-		}
-		throw new IllegalArgumentException("No two sum Solution");
 	}
 
 }
